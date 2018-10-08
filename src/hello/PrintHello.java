339568dfc83711812870bc5dff2576bdbc631a71
jeejon <jeejon3430@nate.com>
2018-10-09 02:59:30 +0900

    Initial commit

---
+++ src/hello/PrintHello.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class PrintHello {
+    public void printHello() {
+        System.out.println("hello");
+
+    }
+}
---
+++ src/hello/PrintHelloTest.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+
+        PrintHello printHello = new PrintHello();
+        printHello.printHello();
+        //System.out.println();
+    }
+}
